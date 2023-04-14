d296aa07b07953ffe129312053d5c66bc17cc1b8
Hyeseon Lee <hyesun1999@gmail.com>
2023-04-15 02:07:46 +0900

    cardslotboard ui 완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import ActionBoard from './components/ActionBoard';
-import CardSlot from './components/CardSlot';
 import { useState } from 'react';
 import CardSlotBoard from './components/CardSlotBoard';
+
 
 function App() {
   const [isCardSlotOpen, setIsCardSlotOpen] = useState(false);
@@ -10,19 +10,11 @@ function App() {
   const closeCardSlot = ()=>setIsCardSlotOpen(false)
 
   return (
-    <div className='static'>
+    <div>
       <ActionBoard />
       
       <div className='w-max h-max bg-red-300' onClick={openCardSlot}>주요설비</div>
-      <div className='flex flex-row'>
-        <div className='basis-1/5 bg-yellow-300'></div>
-        <div className='basis-1/5 bg-yellow-300'></div>
-        <div className='basis-1/5 bg-yellow-300'></div>
-        <div className='basis-1/5 bg-yellow-300'></div>
-        <div className='basis-1/5 bg-yellow-300'></div>
 
-      </div>
-      
       {isCardSlotOpen && 
       <CardSlotBoard img="../../public/img/majors_bg.jpg" col="5" row="2" close={closeCardSlot} />
       }

0820ef720a510ba2bb8fe76c9c2bb5af5b997ab1
dongree <dongwoo0307@naver.com>
2023-04-18 13:58:56 +0900

    농장판 레이아웃 구성

---
+++ src/components/Land.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Land() {
+  return (
+    <div
+      className={
+        'basis-5/19 bg-green-500 aspect-square bg-clip-content	p-1 h-24 cursor-pointer'
+      }
+    ></div>
+  );
+}

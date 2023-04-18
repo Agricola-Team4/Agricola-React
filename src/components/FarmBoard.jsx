76a7310178d43f0e2f383cc07ab7e0e86ad7b88f
dongree <dongwoo0307@naver.com>
2023-04-18 17:03:05 +0900

    레이아웃 재구성

--- src/components/FarmBoard.jsx
+++ src/components/FarmBoard.jsx
@@ -4,7 +4,7 @@ import Land from './Land';
 
 export default function FarmBoard() {
   return (
-    <section className="bg-blue-400 basis-8/12 mb-1 flex flex-wrap">
+    <section className="bg-blue-400 basis-7/12 mb-1 flex flex-wrap">
       <div className={'basis-1/19'}></div>
       <Fence ratio="basis-5/19" height="h-5" />
       <div className={'basis-1/19'}></div>
--- src/components/Prompt.jsx
+++ src/components/Prompt.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
 export default function Prompt() {
-  return <div className="bg-orange-200 h-20"></div>;
+  return <div className="bg-orange-200 basis-1/12"></div>;
 }

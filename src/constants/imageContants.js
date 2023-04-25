f5f9c19b3a3afe3301d5175ae1af1b4803344264
Hyeseon Lee <hyesun1999@gmail.com>
2023-04-25 16:43:27 +0900

    image 경로 constant로 뺌

---
+++ src/constants/imageContants.js
@@ -0,0 +1,22 @@
+const jobImages = {
+  job_a087: require("../assets/images/a087.jpg"),
+  job_a088: require("../assets/images/a088.jpg"),
+  job_a090: require("../assets/images/a090.jpg"),
+  job_a111: require("../assets/images/a111.jpg"),
+  job_a114: require("../assets/images/a114.jpg"),
+  job_a116: require("../assets/images/a116.jpg"),
+  job_a119: require("../assets/images/a119.jpg"),
+  job_b087: require("../assets/images/b087.jpg"),
+  job_b091: require("../assets/images/b091.jpg"),
+  job_b095: require("../assets/images/b095.jpg"),
+  job_b102: require("../assets/images/b102.jpg"),
+  job_b104: require("../assets/images/b104.jpg"),
+  job_b107: require("../assets/images/b107.jpg"),
+  job_b108: require("../assets/images/b108.jpg"),
+};
+
+const majorImages = {};
+
+const subImages = {};
+
+export { jobImages, majorImages, subImages };

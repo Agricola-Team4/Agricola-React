e37c49cdca5c34986fbc5c7e3cf0cd145776a526
Hyeseon Lee <hyesun1999@gmail.com>
2023-05-02 02:36:51 +0900

    방(Room) 밭(Field) 우리(Pen) 컴포넌트를 개발했으며, 말이 놓이는 모습을 표현하는 Farmer는 재사용 되는 곳이 많을 것으로 예상되어 별도의 컴포넌트로 제작함

---
+++ src/components/Farmer.jsx
@@ -0,0 +1,6 @@
+import React from "react";
+import { playerRed } from "../constants/imageContants";
+
+export default function Farmer() {
+  return <img className="w-5/12" src={playerRed.farmer} alt="농부" />;
+}

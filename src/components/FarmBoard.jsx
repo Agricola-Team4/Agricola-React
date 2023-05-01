5175f505c214c028b618f75c132147b24f7cebcd
Hyeseon Lee <hyesun1999@gmail.com>
2023-05-02 02:51:01 +0900

    보여주기 위해서 가능성 있는 위치에 방/밭/우리 재배치

--- src/components/FarmBoard.jsx
+++ src/components/FarmBoard.jsx
@@ -15,23 +15,9 @@ export default function FarmBoard() {
       <Fence ratio="basis-1/28" isVertical={true} />
       <Land isRoom={true} isField={false} isPen={false} /> {/* 1 */}
       <Fence ratio="basis-1/28" isVertical={true} />
-      <Land isRoom={false} isField={true} isPen={false} /> {/* 2 */}
+      <Land isRoom={true} isField={false} isPen={false} /> {/* 2 */}
       <Fence ratio="basis-1/28" isVertical={true} />
-      <Land isRoom={false} isField={false} isPen={true} /> {/* 3 */}
-      <Fence ratio="basis-1/28" isVertical={true} />
-      <div className="basis-1/28"></div>
-      <Fence ratio="basis-8/28" isVertical={false} />
-      <div className="basis-1/28"></div>
-      <Fence ratio="basis-8/28" isVertical={false} />
-      <div className="basis-1/28"></div>
-      <Fence ratio="basis-8/28" isVertical={false} />
-      <div className="basis-1/28"></div>
-      <Fence ratio="basis-1/28" isVertical={true} />
-      <Land />
-      <Fence ratio="basis-1/28" isVertical={true} />
-      <Land />
-      <Fence ratio="basis-1/28" isVertical={true} />
-      <Land />
+      <Land /> {/* 3 */}
       <Fence ratio="basis-1/28" isVertical={true} />
       <div className="basis-1/28"></div>
       <Fence ratio="basis-8/28" isVertical={false} />
@@ -44,6 +30,20 @@ export default function FarmBoard() {
       <Land />
       <Fence ratio="basis-1/28" isVertical={true} />
       <Land />
+      <Fence ratio="basis-1/28" isVertical={true} />
+      <Land isRoom={false} isField={true} isPen={false} />
+      <Fence ratio="basis-1/28" isVertical={true} />
+      <div className="basis-1/28"></div>
+      <Fence ratio="basis-8/28" isVertical={false} />
+      <div className="basis-1/28"></div>
+      <Fence ratio="basis-8/28" isVertical={false} />
+      <div className="basis-1/28"></div>
+      <Fence ratio="basis-8/28" isVertical={false} />
+      <div className="basis-1/28"></div>
+      <Fence ratio="basis-1/28" isVertical={true} />
+      <Land />
+      <Fence ratio="basis-1/28" isVertical={true} />
+      <Land isRoom={false} isField={false} isPen={true} />
       <Fence ratio="basis-1/28" isVertical={true} />
       <Land />
       <Fence ratio="basis-1/28" isVertical={true} />

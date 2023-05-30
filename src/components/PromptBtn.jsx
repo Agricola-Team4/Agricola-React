1272a39b44fadcac7900054dca6c9b80654473a2
dongree <dongwoo0307@naver.com>
2023-05-30 17:23:45 +0900

    react query test

--- src/components/PromptBtn.jsx
+++ src/components/PromptBtn.jsx
@@ -1,8 +1,14 @@
+import axios from 'axios';
 import React from 'react';
 
 export default function PromptBtn({ text }) {
   return (
-    <div className="px-2 py-1 mx-0.5 border-2 border-black rounded-lg cursor-pointer">
+    <div
+      className="px-2 py-1 mx-0.5 border-2 border-black rounded-lg cursor-pointer"
+      onClick={() => {
+        axios.patch('http://localhost:3001/resource/1', { wood: 4 });
+      }}
+    >
       {text}
     </div>
   );

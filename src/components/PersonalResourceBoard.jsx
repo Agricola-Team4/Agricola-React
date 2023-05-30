d933a6d9c83ca2daab32f2f5a0173f95645e2302
dongree <dongwoo0307@naver.com>
2023-05-30 17:08:12 +0900

    #1 구조분해할당 in reactquery

--- src/components/PersonalResourceBoard.jsx
+++ src/components/PersonalResourceBoard.jsx
@@ -1,26 +1,80 @@
-import React from "react";
-import { Avatar } from "@material-tailwind/react";
-import sheep from "../assets/images/sheep_icon.png";
-import cow from "../assets/images/cow_icon.png";
-import reed from "../assets/images/reed_icon.png";
-import boar from "../assets/images/boar_icon.png";
-import grain from "../assets/images/grain_icon.png";
-import soil from "../assets/images/soil_icon.png";
-import stone from "../assets/images/stone_icon.png";
-import vege from "../assets/images/vege_icon.png";
-import wood from "../assets/images/wood_icon.png";
-import food from "../assets/images/food_icon.png";
-import profile from "../assets/images/korea.png";
-import farmer_red from "../assets/images/farmer_icon_red.png";
-import fence_red from "../assets/images/fence_icon_red.png";
-import stable_red from "../assets/images/stable_icon_red.png";
+import React from 'react';
+import { Avatar } from '@material-tailwind/react';
+import sheep_img from '../assets/images/sheep_icon.png';
+import cow_img from '../assets/images/cow_icon.png';
+import reed_img from '../assets/images/reed_icon.png';
+import boar_img from '../assets/images/boar_icon.png';
+import grain_img from '../assets/images/grain_icon.png';
+import soil_img from '../assets/images/soil_icon.png';
+import stone_img from '../assets/images/stone_icon.png';
+import vege_img from '../assets/images/vege_icon.png';
+import wood_img from '../assets/images/wood_icon.png';
+import food_img from '../assets/images/food_icon.png';
+import profile_img from '../assets/images/korea.png';
+import farmer_red_img from '../assets/images/farmer_icon_red.png';
+import fence_red_img from '../assets/images/fence_icon_red.png';
+import stable_red_img from '../assets/images/stable_icon_red.png';
+import axios from 'axios';
+import { useQuery } from '@tanstack/react-query';
 
 export default function PersonalResourceBoard() {
+  // console.log(sheep);
+  const {
+    isLoading,
+    error,
+    data: {
+      sheep,
+      cow,
+      reed,
+      boar,
+      grain,
+      soil,
+      stone,
+      vege,
+      wood,
+      food,
+      profile,
+      farmer_red,
+      fence_red,
+      stable_red,
+    } = {},
+  } = useQuery(
+    ['resource'],
+    async () => {
+      console.log('🙇🏻‍♀️afternoon fetching ...🙇🏻‍♀️');
+      return await axios
+        .get('http://localhost:3001/resource/1')
+        .then(res => res.data);
+
+      // return result;
+    },
+    {
+      staleTime: 1000 * 60,
+    }
+  );
+
+  // const {
+  //   sheep,
+  //   cow,
+  //   reed,
+  //   boar,
+  //   grain,
+  //   soil,
+  //   stone,
+  //   vege,
+  //   wood,
+  //   food,
+  //   profile,
+  //   farmer_red,
+  //   fence_red,
+  //   stable_red,
+  // } = resource;
+
   return (
     <>
       <div className="basis-2/12 mb-1 flex flex-row justify-center bg-brown-100">
         <div className="basis-2/12 flex flex-col items-center pt-4 border-r-2">
-          <Avatar src={profile} alt="avatar" size="md" variant="circular" />
+          <Avatar src={profile_img} alt="avatar" size="md" variant="circular" />
           <p className="pt-2">이름</p>
         </div>
 
@@ -30,45 +84,45 @@ export default function PersonalResourceBoard() {
               <div className="basis-1/5 flex items-center justify-center flex-col mx-2">
                 <img
                   className="aspect-square"
-                  src={wood}
+                  src={wood_img}
                   alt=""
-                  style={{ height: "38px" }}
+                  style={{ height: '38px' }}
+                />
+                <p className="">{wood}</p>
+              </div>
+              <div className="basis-1/5 flex items-center justify-center flex-col mx-2">
+                <img
+                  className="aspect-square"
+                  src={soil_img}
+                  alt=""
+                  style={{ height: '38px' }}
                 />
                 <p className="">15</p>
               </div>
               <div className="basis-1/5 flex items-center justify-center flex-col mx-2">
                 <img
                   className="aspect-square"
-                  src={soil}
+                  src={stone_img}
                   alt=""
-                  style={{ height: "38px" }}
+                  style={{ height: '38px' }}
                 />
                 <p className="">15</p>
               </div>
               <div className="basis-1/5 flex items-center justify-center flex-col mx-2">
                 <img
                   className="aspect-square"
-                  src={stone}
+                  src={reed_img}
                   alt=""
-                  style={{ height: "38px" }}
+                  style={{ height: '38px' }}
                 />
                 <p className="">15</p>
               </div>
               <div className="basis-1/5 flex items-center justify-center flex-col mx-2">
                 <img
                   className="aspect-square"
-                  src={reed}
+                  src={food_img}
                   alt=""
-                  style={{ height: "38px" }}
-                />
-                <p className="">15</p>
-              </div>
-              <div className="basis-1/5 flex items-center justify-center flex-col mx-2">
-                <img
-                  className="aspect-square"
-                  src={food}
-                  alt=""
-                  style={{ height: "38px" }}
+                  style={{ height: '38px' }}
                 />
                 <p className="">15</p>
               </div>
@@ -77,45 +131,45 @@ export default function PersonalResourceBoard() {
               <div className="basis-1/5 flex items-center justify-center flex-col mx-2">
                 <img
                   className="aspect-square"
-                  src={sheep}
+                  src={sheep_img}
                   alt=""
-                  style={{ height: "38px" }}
+                  style={{ height: '38px' }}
                 />
                 <p className="">15</p>
               </div>
               <div className="basis-1/5 flex items-center justify-center flex-col mx-2">
                 <img
                   className="aspect-square"
-                  src={boar}
+                  src={boar_img}
                   alt=""
-                  style={{ height: "38px" }}
+                  style={{ height: '38px' }}
                 />
                 <p className="">15</p>
               </div>
               <div className="basis-1/5 flex items-center justify-center flex-col mx-2">
                 <img
                   className="aspect-square"
-                  src={cow}
+                  src={cow_img}
                   alt=""
-                  style={{ height: "38px" }}
+                  style={{ height: '38px' }}
                 />
                 <p className="">15</p>
               </div>
               <div className="basis-1/5 flex items-center justify-center flex-col mx-2">
                 <img
                   className="aspect-square"
-                  src={grain}
+                  src={grain_img}
                   alt=""
-                  style={{ height: "38px" }}
+                  style={{ height: '38px' }}
                 />
                 <p className="">15</p>
               </div>
               <div className="basis-1/5 flex items-center justify-center flex-col mx-2">
                 <img
                   className="aspect-square"
-                  src={vege}
+                  src={vege_img}
                   alt=""
-                  style={{ height: "38px" }}
+                  style={{ height: '38px' }}
                 />
                 <p className="">15</p>
               </div>
@@ -127,18 +181,18 @@ export default function PersonalResourceBoard() {
               <div className="basis-1/2 flex items-center justify-center flex-col mx-2">
                 <img
                   className="aspect-square"
-                  src={farmer_red}
+                  src={farmer_red_img}
                   alt=""
-                  style={{ height: "38px" }}
+                  style={{ height: '38px' }}
                 />
                 <p className="">15</p>
               </div>
               <div className="basis-1/2 flex items-center justify-center flex-col mx-2">
                 <img
                   className="aspect-square"
-                  src={farmer_red}
+                  src={farmer_red_img}
                   alt="신생아"
-                  style={{ height: "38px" }}
+                  style={{ height: '38px' }}
                 />
                 <p className="">15</p>
               </div>
@@ -147,18 +201,18 @@ export default function PersonalResourceBoard() {
               <div className="basis-1/2 flex items-center justify-center flex-col mx-2">
                 <img
                   className="aspect-square"
-                  src={fence_red}
+                  src={fence_red_img}
                   alt=""
-                  style={{ height: "38px" }}
+                  style={{ height: '38px' }}
                 />
                 <p className="">15</p>
               </div>
               <div className="basis-1/2 flex items-center justify-center flex-col mx-2">
                 <img
                   className="aspect-square"
-                  src={stable_red}
+                  src={stable_red_img}
                   alt=""
-                  style={{ height: "38px" }}
+                  style={{ height: '38px' }}
                 />
                 <p className="">15</p>
               </div>

4bca9f189e8609903458367e4caa1c643d8d71d5
dongree <dongwoo0307@naver.com>
2023-06-06 01:39:46 +0900

    useResource 이슈 해결

--- src/hooks/useResource.jsx
+++ src/hooks/useResource.jsx
@@ -1,23 +1,30 @@
-import { useQuery, useMutation, useQueryClient } from "@tanstack/react-query";
-import { getAllResource, updateOneResource } from "../api/agricola";
-import { updateBabyState } from "../api/agricola";
+import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
+import { getAllResource, updateOneResource } from '../api/agricola';
+import { updateBabyState } from '../api/agricola';
+import { useAuthContext } from '../context/AuthContext';
+import { get } from 'http';
 
-export default function useResource({ pid }) {
-  console.log("pid:", pid);
+export default function useResource() {
+  const { pid } = useAuthContext();
   const queryClient = useQueryClient();
 
-  const resourceQuery = useQuery(["resource"], getAllResource(pid));
+  const resourceQuery = useQuery(
+    ['resource', pid || ''],
+    () => getAllResource(pid),
+    { enabled: !!pid }
+  );
+  console.log(pid);
 
   // update Resource 인자 넣어서 하는 방법 진행해야함.
   const updateResource = useMutation(
     (pid, rid, num) => updateOneResource(pid, rid, num),
     {
-      onSuccess: () => queryClient.invalidateQueries(["resource"]), // queryKey 유효성 제거
+      onSuccess: () => queryClient.invalidateQueries(['resource']), // queryKey 유효성 제거
     }
   );
 
   const updateBaby = useMutation(() => updateBabyState(), {
-    onSuccess: () => queryClient.invalidateQueries(["resource"]), // queryKey 유효성 제거
+    onSuccess: () => queryClient.invalidateQueries(['resource']), // queryKey 유효성 제거
   });
 
   return { resourceQuery, updateResource, updateBaby };

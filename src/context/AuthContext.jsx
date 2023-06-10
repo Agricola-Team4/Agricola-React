a2a7d7e3b042a0f6a3a4ac5ed2b753b7f078888f
dongree <dongwoo0307@naver.com>
2023-06-10 22:57:29 +0900

    숲 take action 완료

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
-import axios from "axios";
-import { createContext, useContext, useEffect, useState } from "react";
+import axios from 'axios';
+import { createContext, useContext, useEffect, useState } from 'react';
 
 export const AuthContext = createContext();
 
@@ -10,14 +10,12 @@ export function AuthContextProvider({ children }) {
   const [isAbActive, setIsAbActive] = useState(true);
   const [isCsActive, setIsCsActive] = useState(false);
 
-  // useEffect(() => {
-  //   // player 정보 가져오기 http://3.36.7.233:3000/player
-  //   axios
-  //     .get("http://3.36.7.233:3000/player/choose_first_player")
-  //     .then((res) => {
-  //       console.log("첫번째 플레이어 뽑습니다", res.data);
-  //     });
-  // }, []);
+  useEffect(() => {
+    // player 정보 가져오기 http://3.36.7.233:3000/player
+    axios.get('http://3.36.7.233:3000/player/choose_first_player').then(res => {
+      console.log('첫번째 플레이어 뽑습니다', res.data);
+    });
+  }, []);
 
   return (
     <AuthContext.Provider

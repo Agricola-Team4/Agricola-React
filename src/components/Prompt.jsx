b3e61a4067975c83937cc6f80723b0f88d3fd648
Hyeseon Lee <hyesun1999@gmail.com>
2023-06-11 08:40:54 +0900

    농장확장 take action 로직 작성, 납품팔이 버튼 : round_end api , 낚시 : choose_first_player api , 갈대밭 : initial api 임의로 만들어둠

--- src/components/Prompt.jsx
+++ src/components/Prompt.jsx
@@ -1,13 +1,12 @@
-import React from 'react';
-import PromptBtn from './PromptBtn';
-import { login } from '../api/agricola';
-import { useAuthContext } from '../context/AuthContext';
-import { useBackgroundContext } from '../context/BackgroundContext';
+import React from "react";
+import PromptBtn from "./PromptBtn";
+import { login } from "../api/agricola";
+import { useAuthContext } from "../context/AuthContext";
+import { useBackgroundContext } from "../context/BackgroundContext";
 
 export default function Prompt() {
   const { pid, setPid } = useAuthContext();
   const { prompt } = useBackgroundContext();
-  console.log(prompt);
   return (
     <section className="bg-yellow-100 rounded-lg h-14 my-1 flex justify-center items-center text-sm font-semibold z-0">
       <p className="mr-1">{prompt.message}</p>

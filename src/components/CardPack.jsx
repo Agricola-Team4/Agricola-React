301b3d873865a16d0cd4984f408a329ff7385dec
dongree <dongwoo0307@naver.com>
2023-06-11 22:16:14 +0900

    margin padding 조정

--- src/components/CardPack.jsx
+++ src/components/CardPack.jsx
@@ -6,7 +6,7 @@ export default function CardPack({ text, bgColor, margin, pid }) {
 
   return (
     <div
-      className={`basis-1/2 rounded-lg bg-clip-content ${margin} mb-1 border-1 border-black flex justify-center items-center font-bold cursor-pointer text-sm ${bgColor} z-0`}
+      className={`basis-1/2 rounded-lg  ${margin} mb-1 border-1 border-black flex justify-center items-center font-bold cursor-pointer text-sm ${bgColor} z-0 py-2 my-2`}
       onClick={
         {
           1: text === '보유카드' ? openP1HaveSlot : openP1ActSlot,
--- src/components/Prompt.jsx
+++ src/components/Prompt.jsx
@@ -1,14 +1,14 @@
-import React from "react";
-import PromptBtn from "./PromptBtn";
-import { login } from "../api/agricola";
-import { useAuthContext } from "../context/AuthContext";
-import { useBackgroundContext } from "../context/BackgroundContext";
+import React from 'react';
+import PromptBtn from './PromptBtn';
+import { login } from '../api/agricola';
+import { useAuthContext } from '../context/AuthContext';
+import { useBackgroundContext } from '../context/BackgroundContext';
 
 export default function Prompt() {
   const { pid, setPid } = useAuthContext();
   const { prompt } = useBackgroundContext();
   return (
-    <section className="bg-yellow-100 rounded-lg h-14 my-1 flex justify-center items-center text-sm font-semibold z-0">
+    <section className="bg-yellow-100 rounded-lg h-14 mt-1 mb-2 flex justify-center items-center text-sm font-semibold z-0">
       <p className="mr-1">{prompt.message}</p>
       {prompt.buttons.length !== 0 &&
         prompt.buttons.map((item, index) => (

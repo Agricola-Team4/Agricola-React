6ce87304cec40f9afb4621ba65f1878ffd44e7ba
HyeseonLee <hyesun1999@gmail.com>
2024-01-25 19:49:12 +0900

    fix: end point 변경

--- src/context/WebSocketContext.jsx
+++ src/context/WebSocketContext.jsx
@@ -1,5 +1,5 @@
-import { useQueryClient } from '@tanstack/react-query';
-import React, { createContext, useContext, useEffect, useState } from 'react';
+import { useQueryClient } from "@tanstack/react-query";
+import React, { createContext, useContext, useEffect, useState } from "react";
 
 export const WebSocketContext = createContext();
 
@@ -9,27 +9,25 @@ export function WebSocketProvider({ children }) {
   const queryClient = useQueryClient();
 
   useEffect(() => {
-    const newSocket = new WebSocket(
-      'ws://3.36.7.233:3000/ws/agricola/player_1/'
-    );
+    const newSocket = new WebSocket("ws://3.36.227.50/ws/agricola/player_1/");
 
     newSocket.onopen = () => {
-      console.log('WebSocket connection established');
+      console.log("WebSocket connection established");
     };
 
-    newSocket.onmessage = res => {
+    newSocket.onmessage = (res) => {
       console.log(res);
-      queryClient.invalidateQueries(['actionBoard']);
-      queryClient.invalidateQueries(['farmBoard']);
-      queryClient.invalidateQueries(['resource']);
-      queryClient.invalidateQueries(['haveCardData']);
-      queryClient.invalidateQueries(['majorCardData']);
-      queryClient.invalidateQueries(['actCardData']);
-      queryClient.invalidateQueries(['firstPlayer']);
+      queryClient.invalidateQueries(["actionBoard"]);
+      queryClient.invalidateQueries(["farmBoard"]);
+      queryClient.invalidateQueries(["resource"]);
+      queryClient.invalidateQueries(["haveCardData"]);
+      queryClient.invalidateQueries(["majorCardData"]);
+      queryClient.invalidateQueries(["actCardData"]);
+      queryClient.invalidateQueries(["firstPlayer"]);
     };
 
     newSocket.onclose = () => {
-      console.log('WebSocket connection closed');
+      console.log("WebSocket connection closed");
     };
 
     setSocket(newSocket);
